f8a2f4226b0419509a2afcac977073d0f82b0d40
RajDaBest <imagination12344321.com@gmail.com>
2024-08-16 20:57:26 +0530

    next commit

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,21 +1,38 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
+  @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
-          Text('Pranav Chutiya Hu'),
-          Text('Uski maa randi hai'),
+          Text('The chart'),
+          Text('The expenses list'),
         ],
       ),
     );
